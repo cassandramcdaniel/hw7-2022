a02330dd560f804f201cbc7ad24a4dbe3d851db4
cassandramcdaniel <112428043+cassandramcdaniel@users.noreply.github.com>
2022-11-08 13:16:31 -0500

    Add files via upload

---
+++ HW 7/hw7-2021-main/video.js
@@ -0,0 +1,67 @@
+var video;
+// LOAD
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
+	video.autoplay = false
+	video.loop = false
+	console.log("Auto play is set to "+video.autoplay)
+	console.log("Loop is set to "+video.autoplay)
+});
+// PLAY
+document.querySelector("#play").addEventListener("click", function() {
+console.log("Play Video");
+document.querySelector("#volume").innerHTML = video.volume *100 + "%"
+video.play()
+});
+// PAUSE
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+//SLOWDOWN
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow down Video");
+	video.playbackRate*=.90
+	console.log("Speed is", video.playbackRate)
+});
+//SPEEDUP
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed up Video");
+	video.playbackRate/=.90
+	console.log("Speed is", video.playbackRate)
+});
+//SKIP
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip ahead");
+	video.currentTime += 10
+	if (video.currentTIme >= video.duration)
+		video.currentTime = 0
+	console.log("Video current time is", video.currentTime)
+});
+
+//MUTE
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == true){
+		console.log("Unmute")
+		video.muted = false
+		this.innerHTML = "Mute"
+	}
+	else{
+		console.log("Mute")
+		video.muted = true
+		this.innerHTML = "Unmute"
+
+
+	}
+});
+
+//SLIDER
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("The current value is, video.volume")
+	video.volume = this.value/100
+	console.log("The current value is", video.volume)
+	console.log(document.querySelector("#volume"))
+	document.querySelector("#volume)").innerHTML = vide0.volume * 100 + ""
+
+})
